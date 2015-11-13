b6e51c51aed0302c191953cf307068db4f6fe453
Quinn Tran <ssquinntran@berkeley.edu>
2015-11-12 22:45:55 -0800

    questions

--- lang_vec.py
+++ lang_vec.py
@@ -1,27 +1,3 @@
-"""
-We can also sum these different language vectors into a
-single vector and use it for finding words.  I think we
-should do that next.  Do you think you can change the
-program so that it produces a language vector that is
-the sum of six language vectors, one that is based on
-single letters, another that is based on bigrams, third
-based on trigrams, fourth on tetragrams, fifth on
-pentagrams, and the sixth based on hexagrams.  You
-can use Alice in Wonderland for data:
-
-  http://www.gutenberg.org/ebooks/11
-
-Remove all non-letters from the text and make all
-letters the same case (upper of lower).  That should be
-about 100,000 letters.
-
-How this combination vector can be used to find the
-number of different letters, bigrams, trigrams, etc. in
-the text will be the next exercise.  You can start
-thinking about it.
-
-generate_text.py
-"""
 import random_idx
 import utils
 import numpy as np
@@ -32,6 +8,7 @@ import matplotlib.pyplot as plt
 k = 5000
 # assuming that cluster_sizes is mapping to n-gram size
 # cluster_sz in random_idx referring to specific element (int) in cluster_sizes, array
+
 cluster_sizes = [1, 2, 3, 4, 5, 6, 7, 8]
 ordered = 1
 #alph = 'abc' 
@@ -44,6 +21,7 @@ def gen_lets(N=N,k=k):
     RI_letters = random_idx.generate_letter_id_vectors(N,k)
     return RI_letters
 
+# how apply text_vector from Alice text to lang_vector which doesn't seem to use it as input?
 def create_lang_vec(N=N,k=k, cluster_sizes):
     print "generating english vector of cluster size", cluster_sz
     total_eng = np.zeros((1,N))
@@ -63,7 +41,12 @@ def read_text(n):
 	#	f.write(str(max_cluster_size))
 
 if __name__ == "__main__":
-	create_lang_vec(cluster_sizes)
-
+    # generate RI vector for "text_name"
+    # assumes text_name has .txt
+    text_vector = generate_RI_text(N, RI_letters, cluster_sz, ordered, "AliceInWonderland.txt", alph=alphabet):
+        
+def generate_RI_lang(N,RI_letters, cluster_sz, ordered, languages=None):
+def generate_RI_text_words(N, RI_letters, text_name, alph=alphabet):
+def generate_RI_str(N, RI_letters, cluster_sz, ordered, string, alph=alphabet):
 
 
